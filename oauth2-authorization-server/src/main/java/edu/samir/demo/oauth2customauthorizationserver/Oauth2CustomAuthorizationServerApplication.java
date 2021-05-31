0b16e7a2a5eaa91a0a3c91401951a4bdef17e352
Samir <samir.guemri@gmx.fr>
2021-05-31 19:36:42 +0200

    Add Oauth2CustomAuthorizationServer project

---
+++ oauth2-authorization-server/src/main/java/edu/samir/demo/oauth2customauthorizationserver/Oauth2CustomAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package edu.samir.demo.oauth2customauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2CustomAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2CustomAuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ oauth2-authorization-server/src/test/java/edu/samir/demo/oauth2customauthorizationserver/Oauth2CustomAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.samir.demo.oauth2customauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2CustomAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
